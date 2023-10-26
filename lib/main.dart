f2d009d1c29abcc2d03badcdce23ab082e3d1381
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-10-26 23:21:16 +0200

    Big Change

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const WochenplanerApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WochenplanerApp extends StatelessWidget {
+  const WochenplanerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Kalender'),
+      title: 'Wochenplaner',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WochenplanerPage(),
+    );
+  }
+}
+
+class WochenplanerPage extends StatefulWidget {
+  const WochenplanerPage({super.key});
+
+  @override
+  _WochenplanerPageState createState() => _WochenplanerPageState();
+}
+
+class _WochenplanerPageState extends State<WochenplanerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wochenplaner'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hier könnte Ihr Wochenplan stehen.',
+            ),
+          ],
         ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              final DateTimeRange? picked = await showDateRangePicker(
-                context: context,
-                firstDate: DateTime(2023),
-                lastDate: DateTime(2024),
-              );
-              // ignore: avoid_print
-              if (picked != null) print(picked);
-            },
-            child: const Text('Datum auswählen'),
-          ),
-        ),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Aufgabe hinzufügen',
+        child: Icon(Icons.add),
       ),
     );
   }
