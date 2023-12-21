d05eee12902ad5ace39c828a2beb4f53d35cbc74
RaxoTheOne <traxon.bg@gmail.com>
2023-12-21 18:41:50 +0100

    change splashscreen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:my_weekplaner_app/app.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -8,16 +9,18 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  bool _isVisible = false;
-
   @override
   void initState() {
     super.initState();
-    // Startet die Animation nach einer Verzögerung von 2 Sekunden
+
+    // Startet die Animation nach einer Verzögerung von 3 Sekunden
     Timer(Duration(seconds: 3), () {
-      setState(() {
-        _isVisible = true;
-      });
+      // Navigiert zur nächsten Seite
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => MeinHomebildschirm(),
+        ),
+      );
     });
   }
 
@@ -27,44 +30,28 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Stack(
         children: [
           // Hintergrund des Splashscreens
-          Image.asset(
-            'assets/_cf771525-d194-4742-9e45-a413a6604178.jpeg',
-            fit: BoxFit.cover,
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: FlareActor(
+              'assets/your_animation.flr',  // Pfad zu deiner Flare-Animation
+              animation: 'play',  // Name deiner Animation
+              fit: BoxFit.cover,
+            ),
           ),
-          // Animierter orangener Container mit InkWell
+          // Animierter orangener Container
           Align(
             alignment: Alignment.center,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                AnimatedOpacity(
-                  duration: Duration(seconds: 1),
-                  opacity: _isVisible ? 1.0 : 0.0,
-                  child: InkWell(
-                    onTap: () {
-                      // Navigiert zur nächsten Seite
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (context) => MeinHomebildschirm(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      width: 200,
-                      height: 300,
-                      // Hier kannst du weitere Widgets für dein Bild hinzufügen
-                    ),
-                  ),
-                ),
+                // Hier kannst du weitere Widgets für dein Bild hinzufügen
                 SizedBox(height: 20),
                 // ProgressIndicator
-                if (!_isVisible)
-                  CircularProgressIndicator(
-                    backgroundColor: Colors.transparent,
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
-                  ),
+                CircularProgressIndicator(
+                  backgroundColor: Colors.transparent,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
+                ),
               ],
             ),
           ),
