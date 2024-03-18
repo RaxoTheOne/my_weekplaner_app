ce9027cefc251546aa21c9efa57ff5940269dfd6
RaxoTheOne <traxon.bg@gmail.com>
2024-03-18 22:24:36 +0100

    duration back to 3sec

--- lib/Widgets/splashscreen.dart
+++ lib/Widgets/splashscreen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
 
     // Startet die Animation nach einer Verzögerung von 3 Sekunden
-    Timer(Duration(seconds: 2), () {
+    Timer(Duration(seconds: 3), () {
       // Navigiert zur nächsten Seite
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
